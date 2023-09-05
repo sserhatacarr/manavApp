a3a9ff3044714be7c134a64959d02e81e20c960f
Serhat Acar <sserhatacarr@gmail.com>
2023-09-05 15:03:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5.0;
+        double kga, kge, kgd, kgm, kgp, total;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Kaç kilo armut istiyorsunuz ? : ");
+        kga = inp.nextDouble();
+        armut *= kga;
+
+        System.out.print("Kaç kilo elma istiyorsunuz ? : ");
+        kge = inp.nextDouble();
+        elma *= kge;
+
+        System.out.print("Kaç kilo domates istiyorsunuz ? : ");
+        kgd = inp.nextDouble();
+        domates *= kgd;
+
+        System.out.print("Kaç kilo muz istiyorsunuz ? : ");
+        kgm = inp.nextDouble();
+        muz *= kgm;
+
+        System.out.print("Kaç kilo patlıcan istiyorsunuz ? : ");
+        kgp = inp.nextDouble();
+        patlican *= kgp;
+
+        total = armut + elma + domates + muz + patlican;
+        System.out.print("Toplam tutar = " + total);
+
+
+    }
+}
